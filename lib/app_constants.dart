2deb73ffc411cf1a4c3c00832a25fcad30c964f3
takezawa <s.take4173@gmail.com>
2025-04-11 22:26:30 +0900

    Align within constant file

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static const String appName = 'Touch Figure';
-  static const Color circleColor = Colors.blue;
-  static const Color seedColor = Colors.deepPurple;
+  static const double countBackgroundOpacity = 0.7;
   static const Duration countdownDelay = Duration(milliseconds: 1000);
   static const Duration resetDelay = Duration(seconds: 2);
+  static const Color circleBorderColor = Color(0xFFf5f5f5);
+  static const Color circleColor = Colors.blue;
   static const Color countBackgroundColor = Colors.black;
-  static const double countBackgroundOpacity = 0.7;
-  static const Color countTextColor = Colors.white;
   static const Color countdownTextColor = Colors.blue;
+  static const Color countTextColor = Colors.white;
+  static const Color seedColor = Colors.deepPurple;
   static const List<Color> circleColors = [
     Colors.blue,
     Colors.red,
